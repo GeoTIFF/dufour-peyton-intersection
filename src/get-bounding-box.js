cb826250b0eb4b6eb54eb1c38fb13b269fde818f
DanielJDufour <daniel.j.dufour@gmail.com>
2022-02-07 16:57:37 -0500

    updated src/get-bounding-box.js

--- src/get-bounding-box.js
+++ src/get-bounding-box.js
@@ -1,33 +1,29 @@
+const eachPolygon = require("./each-polygon.js");
+
 module.exports = function getBoundingBox(geometry) {
   let xmin, ymin, xmax, ymax;
 
-  if (typeof geometry[0][0] === "number") {
-    const numberOfPoints = geometry.length;
-    xmin = xmax = geometry[0][0];
-    ymin = ymax = geometry[0][1];
-    for (let i = 1; i < numberOfPoints; i++) {
-      const [x, y] = geometry[i];
+  eachPolygon(geometry, polygon => {
+    const ring = polygon[0]; // only want the exterior ring
+    const imax = ring.length - 1;
+
+    let i;
+    if (xmin === undefined) {
+      xmin = xmax = ring[0][0];
+      ymin = ymax = ring[0][1];
+      i = 1;
+    } else {
+      i = 0;
+    }
+
+    for (; i <= imax; i++) {
+      const [x, y] = ring[i];
       if (x < xmin) xmin = x;
       else if (x > xmax) xmax = x;
       if (y < ymin) ymin = y;
       else if (y > ymax) ymax = y;
     }
-  } else {
-    geometry.forEach((part, index) => {
-      const bbox = getBoundingBox(part);
-      if (index == 0) {
-        xmin = bbox.xmin;
-        xmax = bbox.xmax;
-        ymin = bbox.ymin;
-        ymax = bbox.ymax;
-      } else {
-        if (bbox.xmin < xmin) xmin = bbox.xmin;
-        else if (bbox.xmax > xmax) xmax = bbox.xmax;
-        if (bbox.ymin < ymin) ymin = bbox.ymin;
-        else if (bbox.ymax > ymax) ymax = bbox.ymax;
-      }
-    });
-  }
+  });
 
-  return { xmax, xmin, ymax, ymin };
-}
+  return [xmin, ymin, xmax, ymax];
+};
