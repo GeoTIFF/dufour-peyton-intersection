323f3848803f83b70225ce7a4e6cda02f9077e13
DanielJDufour <daniel.j.dufour@gmail.com>
2023-06-17 09:44:16 -0400

    commented partition

--- src/partition.js
+++ src/partition.js
@@ -1,3 +1,11 @@
+/**
+ * @name partition
+ * @description break items into two groups based on
+ * whether they pass or fail a provided filter function
+ * @param {Array} array
+ * @param {Function} filter function
+ * @returns {Array} partitioned items - [array_passed, array_unpassed]
+ */
 module.exports = function partition(array, filter) {
   const passed = [];
   const unpassed = [];
