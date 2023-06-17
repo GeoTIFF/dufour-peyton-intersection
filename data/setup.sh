5fc869c030348df71cfc6e6d1b9859e01ce624cb
DanielJDufour <daniel.j.dufour@gmail.com>
2023-06-17 09:55:00 -0400

    updated setup.sh

--- data/setup.sh
+++ data/setup.sh
@@ -3,7 +3,8 @@
 # download from https://github.com/GeoTIFF/test-data/
 wget https://github.com/GeoTIFF/test-data/archive/refs/heads/main.zip -O geotiff-test-data.zip
 unzip -j -o geotiff-test-data.zip "test-data-*/files/*" -d .
-rm geotiff-test-data.zip
+unzip spam2005v3r2_harvested-area_wheat_total.tiff.zip
+rm geotiff-test-data.zip spam2005v3r2_harvested-area_wheat_total.tiff.zip
 
 gdalwarp -t_srs EPSG:4326 gadas-export.png gadas-export-4326.tif
 
